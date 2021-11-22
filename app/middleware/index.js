faf75e9929b4f0662b7411df0adcdc00c2c0c0b0
islamorozobaev <islam260301kg@gmail.com>
2021-11-22 22:10:16 +0600

    first commit (auth, simple model)

---
+++ app/middleware/index.js
@@ -0,0 +1,1 @@
+const authJwt = require('./authJwt')const verifySignUp = require('./verifySignUp')module.exports = {  authJwt,  verifySignUp}
---
+++ app/models/user.model.js
@@ -0,0 +1,1 @@
+module.exports = (sequelize, Sequelize) => {  const User = sequelize.define('users', {    username: {      type: Sequelize.STRING    },    email: {      type: Sequelize.STRING    },    password: {      type: Sequelize.STRING    }  })  return User}
