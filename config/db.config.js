7010207a9c5c3ef3cd33598cf861c8f618ed1c7d
islam00 <islam26030kg@gmaqil.com>
2021-12-15 19:51:28 +0600

    fix db config2

--- config/db.config.js
+++ config/db.config.js
@@ -1,1 +1,1 @@
-module.exports = {  HOST: 'localhost',  USER: 'postgres',  PASSWORD: '123',  DB: 'postgres',  dialect: 'postgres',  port: 5433,  pool: {    max: 5,    min: 0,    acquire: 30000,    idle: 10000  }}
+module.exports = {  HOST: 'localhost',  USER: 'postgres',  PASSWORD: '123',  DB: 'postgres',  dialect: 'postgres',  port: 3310,  pool: {    max: 5,    min: 0,    acquire: 30000,    idle: 10000  }}
